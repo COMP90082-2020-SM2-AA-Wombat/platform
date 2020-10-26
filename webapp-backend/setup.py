d70ba422e8d18aeba795c8c3150a4edc2dc68898
Benedict Ong <benedictong42@gmail.com>
2020-10-26 20:48:15 +1100

    cleanup

--- webapp-backend/setup.py
+++ webapp-backend/setup.py
@@ -14,12 +14,6 @@ with pathlib.Path('requirements.txt').open() as requirements_txt:
 setup(
     name="flaskr",
     version="1.0.0",
-    url="https://flask.palletsprojects.com/tutorial/",
-    license="BSD",
-    maintainer="lol",
-    maintainer_email="contact@palletsprojects.com",
-    description="The basic blog app built in the Flask tutorial.",
-    long_description="readme",
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
