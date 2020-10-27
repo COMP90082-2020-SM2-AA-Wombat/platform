0b3f3fd0f3ad5498fd23be6d24e524a1cea314a2
arielkark <arielkark@gmail.com>
2020-10-27 16:41:08 +1100

    working

--- jupyterhub/jupyterhub_config.py
+++ jupyterhub/jupyterhub_config.py
@@ -28,8 +28,8 @@ c.JupyterHub.admin_access = True
 c.DockerSpawner.network_name = 'jupyterhub_network'
 
 # Docker image for spawned containers
-c.DockerSpawner.image = os.environ.get('DOCKER_NOTEBOOK_DIR') or 'jupyter/scipy-notebook'
-
+# c.DockerSpawner.image = os.environ.get('DOCKER_NOTEBOOK_DIR')# or 'jupyter/scipy-notebook'
+c.DockerSpawner.image = 'custom-lab'
 # Admin users for spawned containers. NB: these users must still be created in the hub, but dont require authorization.
 c.Authenticator.admin_users = {'admin'} 
 
@@ -38,7 +38,8 @@ notebook_dir = '/home/jovyan/work'
 c.DockerSpawner.notebook_dir = notebook_dir
 
 # Mount the real user's Docker volume on the host to the notebook user's notebook directory in the container
-c.DockerSpawner.volumes = { 'jupyterhub-user-{username}': notebook_dir }
+c.DockerSpawner.volumes = { 'jupyterhub-user-{username}': notebook_dir,
+                            'jupyterhub-shared': '/home/jovyan/work/shared' }
 
 # Spawned containers are deleted when stopped but voumes will remain on host
 c.DockerSpawner.remove_containers = True 
